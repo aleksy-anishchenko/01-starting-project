528601395dfe056ac355b9fb37783f6c06a5ebf6
aleksy.anishchenko <aleksy.anishchenko@gmail.com>
2024-08-26 00:24:44 +0700

    Добавляет компонент CoreConcept

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
+import reactImg from "../src/assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "../src/data";
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const descriptions = reactDescriptions[genRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {descriptions} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
+  );
+}
+
+function CoreConcept({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
   );
 }
 
@@ -16,7 +36,15 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {... CORE_CONCEPTS[0]} />
+            <CoreConcept {... CORE_CONCEPTS[1]} />
+            <CoreConcept {... CORE_CONCEPTS[2]} />
+            <CoreConcept {... CORE_CONCEPTS[3]} />     
+          </ul>
+        </section>
       </main>
     </div>
   );
