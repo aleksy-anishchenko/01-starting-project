8de4bb058c37669d8ff232b300c60eec85a08367
aleksy.anishchenko <aleksy.anishchenko@gmail.com>
2024-10-09 01:55:41 +0700

    Добавляет финальное состояние проекта

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,19 +4,16 @@ import './Header.css';
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
-    return Math.floor(Math.random() * (max + 1));
+  return Math.floor(Math.random() * (max + 1));
 }
 
 export default function Header() {
-    const descriptions = reactDescriptions[genRandomInt(2)];
-    return (
-        <header>
-            <img src={reactImg} alt="Stylized atom" />
-            <h1>React Essentials</h1>
-            <p>
-                {descriptions} React concepts you will need for almost any app you are
-                going to build!
-            </p>
-        </header>
-    );
+  const descriptions = reactDescriptions[genRandomInt(2)];
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>{descriptions} React concepts you will need for almost any app you are going to build!</p>
+    </header>
+  );
 }
